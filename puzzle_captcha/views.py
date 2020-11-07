4371006ae7ff8a4bb0ed103fd0a6080c02794d3d
SmituSSupp <mixercool1@gmail.com>
2020-11-07 15:38:20 +0300

    [add] AAAAAAAAAAA

--- puzzle_captcha/views.py
+++ puzzle_captcha/views.py
@@ -17,10 +17,14 @@ def render_puzzle(request):
             print(request.POST[piece.key], looper)
             success = True
             if request.POST[piece.key] != str(looper):
-                message = 'You did not solve the puzzle correctly, please try again.'
+                message = 'You did not solve the puzzle correctly, please try again lad.'
                 success = False
                 break
             looper += 1
+        print(f"{request.POST.get('bot_trap',)}")
+        if request.POST.get('bot_trap',) != 'bot_trap':
+            message = 'GO AWAY LITTLE BOT'
+            success = False
     if success:
         message = 'You solved the puzzle, good job!'
     else:
@@ -28,9 +32,9 @@ def render_puzzle(request):
     return render('puzzle/render_captcha.html', { 'puzzle': puzzle, 'message': message, })
 
 
-def log_user_image_click(request):
-    if request.POST:
-        puzzle = Puzzle.objects.get(key=request.POST['puzzle_key'])
-        print('check click ', request.POST['item_text'])
-
-    return render('puzzle/render_captcha.html', {'puzzle': puzzle, 'message': '', })
+# def log_user_image_click(request):
+#     if request.POST:
+#         puzzle = Puzzle.objects.get(key=request.POST['puzzle_key'])
+#         print('check click ', request.POST['item_text'])
+#
+#     return render('puzzle/render_captcha.html', {'puzzle': puzzle, 'message': '', })
