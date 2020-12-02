5f887c901375d56cdd285aee2ccdc3ff8ae59379
SmituSSupp <mixercool1@gmail.com>
2020-12-02 23:00:51 +0300

    [fix+add] fix log format add new elements

--- test_captcha/views.py
+++ test_captcha/views.py
@@ -20,10 +20,10 @@ def index(request):
         # print(request.headers['Cookie'].split('; ')[1])
         ipaddr = request.META.get('REMOTE_ADDR')
         if ipaddr:
-            logger.info(f"Session opened from {ipaddr} agent - {request.headers['User-Agent']} "
+            logger.info(f"{datetime.date.today()} {datetime.datetime.now().strftime('%H:%M:%S.%f')[:-4]} Session opened from {ipaddr} agent - {request.headers['User-Agent']} "
                         f"on item page index")
         else:
-            logger.info(f"Session opened from {request.headers['Host']} agent - "
+            logger.info(f"{datetime.date.today()} {datetime.datetime.now().strftime('%H:%M:%S.%f')[:-4]} Session opened from {request.headers['Host']} agent - "
                         f"{request.headers['User-Agent']} on page index")
 
     except Exception as e:
@@ -40,10 +40,10 @@ def show_form(request):
         # print(request.headers['Cookie'].split('; ')[1])
         ipaddr = request.META.get('REMOTE_ADDR')
         if ipaddr:
-            logger.info(f"Session opened from {ipaddr} agent - {request.headers['User-Agent']} "
+            logger.info(f"{datetime.date.today()} {datetime.datetime.now().strftime('%H:%M:%S.%f')[:-4]} Session opened from {ipaddr} agent - {request.headers['User-Agent']} "
                         f"on page captcha")
         else:
-            logger.info(f"Session opened from {request.headers['Host']} agent - "
+            logger.info(f"{datetime.date.today()} {datetime.datetime.now().strftime('%H:%M:%S.%f')[:-4]} Session opened from {request.headers['Host']} agent - "
                         f"{request.headers['User-Agent']} on page captcha")
 
     except Exception as e:
@@ -52,10 +52,23 @@ def show_form(request):
         logger.info(request.headers)
 
     form = SubmissionForm(request.POST or None)
+
     if form.is_valid():
-        form.save()
-        print(f'{datetime.date.today()} -  sess closed')
-        logger.info(f'{datetime.date.today()} -  sess closed')
+
+        try:
+            # print(request.headers['Cookie'].split('; ')[1])
+            ipaddr = request.META.get('REMOTE_ADDR')
+            if ipaddr:
+                logger.info(f"{datetime.date.today()} {datetime.datetime.now().strftime('%H:%M:%S.%f')[:-4]} Session closed from {ipaddr} agent - {request.headers['User-Agent']} "
+                            f"on page captcha")
+            else:
+                logger.info(f"{datetime.date.today()} {datetime.datetime.now().strftime('%H:%M:%S.%f')[:-4]} Session closed from {request.headers['Host']} agent - "
+                            f"{request.headers['User-Agent']} on page captcha")
+
+        except Exception as e:
+            logger.info('Cookie error check it')
+            logger.info(request.META)
+            logger.info(request.headers)
 
         return HttpResponse('Thank you for your feedback!')
 
@@ -69,10 +82,10 @@ def log_user_image_click(request):
         #print(request.headers['Cookie'].split('; ')[1])
         ipaddr = request.META.get('REMOTE_ADDR')
         if ipaddr:
-            logger.info(f"click from sess {ipaddr} agent - {request.headers['User-Agent']} "
+            logger.info(f"{datetime.date.today()} {datetime.datetime.now().strftime('%H:%M:%S.%f')[:-4]} click from sess {ipaddr} agent - {request.headers['User-Agent']} "
                         f"on item {item_tag}")
         else:
-            logger.info(f"click from sess {request.headers['Host']} agent - "
+            logger.info(f"{datetime.date.today()} {datetime.datetime.now().strftime('%H:%M:%S.%f')[:-4]} click from sess {request.headers['Host']} agent - "
                         f"{request.headers['User-Agent']} on item {item_tag}")
 
     except Exception as e:
@@ -90,10 +103,10 @@ def log_index_input(request):
         #print(request.headers['Cookie'].split('; ')[1])
         ipaddr = request.META.get('REMOTE_ADDR')
         if ipaddr:
-            logger.info(f"Submit values for index from sess {ipaddr} agent - {request.headers['User-Agent']} "
+            logger.info(f"{datetime.date.today()} {datetime.datetime.now().strftime('%H:%M:%S.%f')[:-4]} Submit values for index from sess {ipaddr} agent - {request.headers['User-Agent']} "
                         f"are {index_res}")
         else:
-            logger.info(f"Submit values for index from sess {request.headers['Host']} agent - "
+            logger.info(f"{datetime.date.today()} {datetime.datetime.now().strftime('%H:%M:%S.%f')[:-4]} Submit values for index from sess {request.headers['Host']} agent - "
                         f"{request.headers['User-Agent']} are {index_res}")
 
     except Exception as e:
